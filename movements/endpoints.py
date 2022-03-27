51a28656988eb606c7dd3531582137172dd81e1f
Fredrik Gyllenhammar <fredrik.gyllenhammar@gmail.com>
2022-03-27 19:35:06 +0200

    Add /healthcheck endpoint

--- movements/endpoints.py
+++ movements/endpoints.py
@@ -3,11 +3,24 @@ from __future__ import annotations
 from datetime import datetime
 
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
+from movements.database import database
 from movements.models import Poop
 
+api_router = APIRouter(prefix='/api')
 poop_router = APIRouter(prefix='/poop')
+api_router.include_router(poop_router)
+
+
+@api_router.get('/healthcheck')
+async def health_check() -> JSONResponse:
+    try:
+        time: str = await database.fetch_val("""SELECT datetime('now')""")
+        return JSONResponse({'message': f'Database connection working, current time is {time}'})
+    except Exception:
+        return JSONResponse(status_code=500, content={'message': 'An error occured, database unreachable'})
 
 
 @poop_router.get('/', response_model=list[Poop])
@@ -31,8 +44,5 @@ class PoopRequest(BaseModel):
 async def create_poop(poop: PoopRequest) -> Poop:
     return await Poop(**poop.dict()).save()
 
-
-api_router = APIRouter(prefix='/api')
-api_router.include_router(poop_router)
 
 __all__ = ['api_router']
